fd2fed28cc5cbf1a3a754ad0253375bf213d9cf1
tale80 <talexandre@ackomas.com>
2024-11-27 10:22:24 +0100

    Add initial project structure and assets
    Added core project files including `_Imports.razor`, `App.razor`, and predefined CSS styles. Also included favicon and app icons for a richer UI experience.

---
+++ RadzenDataFilterSort/Model/VmUser.cs
@@ -0,0 +1,16 @@
+namespace RadzenDataFilterSort.Model;
+
+public class VmUser
+{
+    public string LastName { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public bool IsActive { get; set; }
+    
+    public bool IsAdmin { get; set; }
+    
+    public string UserName { get; set; }
+}
